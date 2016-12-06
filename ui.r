6aab0ebf29eab3389640422a26993602a020a34b
Elena Oleksun <candyflos2011@gmail.com>
2016-12-06 20:49:22 +0200

    Add files via upload

---
+++ ui.r
@@ -0,0 +1,20 @@
+library(shiny)
+
+checkbox <- function(){
+  data <- read.table("data.txt", sep = ";", header = TRUE, encoding = "UTF-8")
+  result <- as.vector(data[,1])
+  names(result) <- result
+  return (result)
+}
+
+shinyUI(pageWithSidebar(
+  headerPanel("Кількість жінок у містах"),
+  
+  sidebarPanel(
+    checkboxGroupInput("city", "Виберіть регіон:", checkbox())
+  ),
+  
+  mainPanel(
+    plotOutput("plot", height = "800px")
+  )
+))
